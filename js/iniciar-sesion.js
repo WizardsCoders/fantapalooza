e969cff8c87772ca9214a01151c2953ea3d90abd
Facu <facundo_leonel_paz63@hotmail.com.ar>
2024-05-21 16:26:53 -0300

    Cambios en el nombre de las variables y además agregué un campo para que se construya en memoria un dinero inicial de usuario en 0

--- js/iniciar-sesion.js
+++ js/iniciar-sesion.js
@@ -1,10 +1,11 @@
 let formulario = document.getElementById("formulario-ingresar"); // Me guardo el formmulario de ingreso en una variable
 formulario.addEventListener("submit", function (event) { // Queda a la espera del boton submit del formulario
     event.preventDefault();                             // Si algo no está bien cancela la ejecución del envío del formulario
-    let usuario = document.getElementById("usuario").value; // Obtengo el valor del campo usuario
+    let nombreUsuario = document.getElementById("nombre-usuario").value; // Obtengo el valor del campo usuario
     let contraseña = document.getElementById("contraseña").value; // Obtengo el valor del campo contraseña
-    if (usuario && contraseña == "usuario") {   // Compruebo que se haya escrito algo en usuario y también que la contraseña que me llegue sea = a la palabra usuario
-      localStorage.setItem("usuario", usuario)  //Guardo en el local storage lo que me haya llegado como nombre de usuario
+    if (nombreUsuario && contraseña == "usuario") {   // Compruebo que se haya escrito algo en usuario y también que la contraseña que me llegue sea = a la palabra usuario
+      localStorage.setItem("nombre-usuario", nombreUsuario)  //Guardo en el local storage lo que me haya llegado como nombre de usuario
+      localStorage.setItem("dinero", "0")   // Agrego una llave valor en el local storage que represente el dinero inicial en $0
       let url = "../index.html";      // Me guardo en una variable la ruta hacia el index
       window.location.href = url;     // Redirecciono hacia el index
     } else {
