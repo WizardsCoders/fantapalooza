9a5efa009dc6e734f8fa3757255dbab284d45a85
NahueeGarcia <nagarcia@frba.utn.edu.ar>
2024-06-27 16:54:58 -0300

    Terminado y probado CRUD parte3

--- app.py
+++ app.py
@@ -1,73 +1,99 @@
+import mysql.connector
 class Usuario:
-    usuarios = []
+    def __init__(self, host, user, password, database):
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.conn.cursor(dictionary=True)
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+            codigo INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(255) NOT NULL,
+            apellido VARCHAR(255) NOT NULL,
+            usuario VARCHAR(255) NOT NULL,
+            imagen_url VARCHAR(255) )''')
+        self.conn.commit()
 
-    def agregar_usuario(self, codigo, nombre, apellido, documento, nombre_usuario, imagen):
-        if(self.consultar_usuario(codigo)):
-            return False
-    
-        nuevo_usuario = {
-            'codigo': codigo,
-            'nombre': nombre,
-            'apellido': apellido,
-            'documento': documento,
-            'nombre_usuario': nombre_usuario,
-            'imagen': imagen
-        }
+    def agregar_usuario(self, nombre, apellido, nombre_usuario, imagen):
 
-        self.usuarios.append(nuevo_usuario)
-        return True
+        sql = "INSERT INTO usuarios (nombre, apellido, usuario, imagen_url) VALUES (%s, %s, %s, %s)"
+        valores = (nombre, apellido, nombre_usuario, imagen)
+
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.lastrowid
 
     def consultar_usuario(self, codigo):
-        encontrado = False
-        i=0
-        while(encontrado == False and i<len(self.usuarios)):
-            if(codigo == self.usuarios[i]['codigo']):
-                encontrado = True
-            
-            i+=1
-        return encontrado
+        # Consultamos un usuario a partir de su código
+        self.cursor.execute(f"SELECT * FROM usuarios WHERE codigo = {codigo}")
+        return self.cursor.fetchone()
 
     #no tiene mucho sentido en nuestro caso pero bueno
-    def modificar_usuario(self, codigo, nuevo_nombre, nuevo_apellido, nuevo_documento, nuevo_nombre_usuario, nueva_imagen):
-        for usuario in self.usuarios:
-            if usuario['codigo'] == codigo:
-                usuario['nombre'] = nuevo_nombre
-                usuario['apellido'] = nuevo_apellido
-                usuario['documento'] = nuevo_documento
-                usuario['nombre_usuario'] = nuevo_nombre_usuario
-                usuario['imagen'] = nueva_imagen
-                return True
-        return False
+    def modificar_usuario(self, codigo, nuevo_nombre, nuevo_apellido, nuevo_nombre_usuario, nueva_imagen):
+        sql = "UPDATE usuarios SET nombre = %s, apellido = %s, usuario = %s, imagen_url = %s WHERE codigo = %s"
+        valores = (nuevo_nombre, nuevo_apellido, nuevo_nombre_usuario, nueva_imagen, codigo)
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+        return self.cursor.rowcount > 0      
 
-    def listar_usuarios(self):
-        print("-" * 50)
-        for usuario in self.usuarios:
+    def mostrar_usuario(self, codigo):
+        #se muestra los datos de un usuario a partir de su código
+        usuario = self.consultar_usuario(codigo)
+        if usuario:
+            print("-" * 40)
             print(f"Código.....: {usuario['codigo']}")
             print(f"Nombre.....: {usuario['nombre']}")
             print(f"Apellido...: {usuario['apellido']}")
-            print(f"Documento..: {usuario['documento']}")
-            print(f"Usuario....: {usuario['nombre_usuario']}")
-            print(f"Imagen.....: {usuario['imagen']}")        
-            print("-" * 50)
+            print(f"Usuario....: {usuario['usuario']}")
+            print(f"Imagen.....: {usuario['imagen_url']}")        
+            print("-" * 40)
+        else:
+            print("Usuario no encontrado.")
 
+    def listar_usuarios(self):
+        self.cursor.execute("SELECT * FROM usuarios")
+        usuarios = self.cursor.fetchall()
+        return usuarios
+    
     def eliminar_usuario(self, codigo):
-        for usuario in self.usuarios:
-            if(usuario['codigo'] == codigo):
-                self.usuarios.remove(usuario)
-                return True
-        return False
-
+        # Eliminamos un producto de la tabla a partir de su código
+        self.cursor.execute(f"DELETE FROM usuarios WHERE codigo = {codigo}")
+        self.conn.commit()
+        return self.cursor.rowcount > 0
 
 #main
-usuarios = Usuario()
-usuarios.agregar_usuario(1,"asd","ger",2345,"pepito", "g1.png") 
-usuarios.agregar_usuario(2,"asd2","ger2",122345,"pepitoG", "g2.png")
-print(usuarios.consultar_usuario(0))
-print(usuarios.consultar_usuario(1))
-usuarios.listar_usuarios()
-"""
-modificar_usuario(1,"asd","gerson",2345,"pepito", "ger.png")
-listar_usuarios()
-"""
+usuarios = Usuario(host='localhost', user='root', password='', database='miapp')
+'''
+usuarios.agregar_usuario("Juan","Perez","pepito", "1.png") 
+usuarios.agregar_usuario("Jose","Sosa", "josesito", "2.png")
+'''
+
+'''
+#ejemplo consulta
+cod_usuario = int(input("Ingrese el código del usuario: "))
+usuario = usuarios.consultar_usuario(cod_usuario)
+if usuario:
+    print(f"Usuario encontrado: {usuario['codigo']} - {usuario['usuario']}")
+else:
+    print(f'Usuario {cod_usuario} no encontrado.')
+
+usuarios.mostrar_usuario(1)
+usuarios.modificar_usuario(1,"Juancito","Perez", "pepito123", "1.png")
+usuarios.mostrar_usuario(1)
+
 print(usuarios.eliminar_usuario(1))
 usuarios.listar_usuarios()
+'''
+lista_usuarios = usuarios.listar_usuarios()
+for usuario in lista_usuarios:
+    print(usuario)
+
+usuarios.agregar_usuario("Alvaro","Escobedo", "alvarito", "3.png")
+
+print("-----------")
+
+lista_usuarios = usuarios.listar_usuarios()
+for usuario in lista_usuarios:
+    print(usuario)
